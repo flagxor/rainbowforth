9fb35afd9370e535892a4b6af14ea92a684c2fbd
Brad Nelson <flagxor@gmail.com>
2008-10-21 07:14:17 +0000

    Flattening things. But may reconsider.

---
+++ ajax/rainbowforth.py
@@ -0,0 +1,103 @@
+import os
+import sys
+from google.appengine.api import users
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+
+
+class Block(db.Model):
+  author = db.UserProperty()
+  category = db.StringProperty()
+  index = db.IntegerProperty()
+  data = db.BlobProperty()
+
+
+class ReadBlock(webapp.RequestHandler):
+  def post(self):
+    user = users.get_current_user()
+    if user:
+      category = self.request.get('category')
+      index = int(self.request.get('index'))
+      query = Block.gql('WHERE author = :author and '
+                        'index = :index and '
+                        'category = :category '
+                        'LIMIT 1',
+                        author=user,
+                        index=index,
+                        category=category)
+      block = query.fetch(1)
+      if block:
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(block[0].data)
+      else:
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write(' ' * 1024)
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+
+
+class WriteBlock(webapp.RequestHandler):
+  def post(self):
+    user = users.get_current_user()
+    if user:
+      category = self.request.get('category')
+      index = int(self.request.get('index'))
+      data = str(self.request.get('data'))
+      query = Block.gql('WHERE author = :author and '
+                        'index = :index and '
+                        'category = :category '
+                        'LIMIT 1',
+                        author=user,
+                        index=index,
+                        category=category)
+      block = query.fetch(1)
+      if block:
+        block[0].data = data
+        block[0].put()
+      else:
+        b = Block()
+        b.category = category
+        b.index = index
+        b.author = user
+        b.data = data
+        b.put()
+      self.redirect('/')
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+
+
+class TestPage(webapp.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if user:
+      path = os.path.join(os.path.dirname(__file__), 'blocktest.html')
+      self.response.out.write(template.render(path, {}))
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+
+
+class MainPage(webapp.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    if user:
+      path = os.path.join(os.path.dirname(__file__), 'rainbowforth.html')
+      self.response.out.write(template.render(path, {}))
+    else:
+      self.redirect(users.create_login_url(self.request.uri))
+
+
+
+def main():
+  application = webapp.WSGIApplication(
+      [('/', MainPage),
+       ('/test', TestPage),
+       ('/read', ReadBlock),
+       ('/write', WriteBlock)],
+      debug=True)
+  run_wsgi_app(application)
+
+
+if __name__ == "__main__":
+  main()
