dcf46346695c173c8ba9473544c731c2589b2c5f
Brad Nelson <flagxor@gmail.com>
2013-04-25 18:39:46 +0000

    Tuning

--- forthsalon_tools/stats.py
+++ forthsalon_tools/stats.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
+import math
 import re
+
+THRESHOLD = 95
 
 known = (
 'x y t push pop dup over 2dup drop swap rot -rot = <> < > <= >= and or not '
@@ -9,14 +12,17 @@ known = (
 
 print 'KNOWN: %d' % len(known)
 
-words = {}
+haikus = []
 
 inside = False
+haiku = []
 for line in open('haiku_snapshot.txt'):
   if line.startswith('Code:'):
     inside = True
     continue
   if line.startswith('--------'):
+    haikus.append(haiku)
+    haiku = []
     inside = False
     continue
   if inside:
@@ -32,17 +38,75 @@ for line in open('haiku_snapshot.txt'):
       rword = word.lower()
       if rword not in known:
         if re.match('^[-]?[0-9\.]+$', rword):
-          #rword = '%.1f' % float(rword)
-          rword = 'number'
+          rword = '%.1f' % float(rword)
         else:
           rword = 'foo'
-      if rword not in words:
-        words[rword] = 0
-      words[rword] += 1
+      haiku.append(rword)
 
-groups = []
-for word in words.keys():
-  groups.append((words[word], word))
-groups = sorted(groups)
-for pair in groups:
-  print pair[0], pair[1]
+
+def mean(items):
+  return float(sum(items)) / float(len(items))
+
+
+def midpoint(items, partition):
+  items = sorted(items)
+  count = len(items)
+  mid = int(count * partition)
+  return items[mid]
+
+
+def median(items):
+  items = sorted(items)
+  count = len(items)
+  if count % 2 == 0:
+    return (items[count / 2] + items[count / 2 + 1]) / 2.0
+  else:
+    return items[count / 2]
+
+
+def standard_deviation(items):
+  m = mean(items)
+  tally = 0
+  for item in items:
+    tally += pow(item - m, 2)
+  return math.sqrt(tally / float(len(items)))
+
+
+def stats(items):
+  return 'range: %g:%g, mean: %g(%g), 50:%g 75:%g %d:%g' % (
+      min(items),
+      max(items),
+      mean(items),
+      standard_deviation(items),
+      median(items),
+      midpoint(items, 0.75),
+      THRESHOLD,
+      midpoint(items, THRESHOLD / 100.0),
+      )
+
+print 'Haiku Size> ' + stats([len(i) for i in haikus])
+
+
+words = set()
+for haiku in haikus:
+  for word in haiku:
+    words.add(word)
+words = sorted(list(words))
+
+
+active = 0
+for word in words:
+  uses = []
+  for haiku in haikus:
+    count = 0
+    for w in haiku:
+      if w == word:
+        count += 1
+    uses.append(count)
+  use90 = midpoint(uses, THRESHOLD / 100.0)
+  active += use90
+  if use90 > 0:
+    print '%s --- %s' % (word, stats(uses))
+
+
+print 'ACTIVE: %d' % active
