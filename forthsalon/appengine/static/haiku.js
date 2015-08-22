accaf77b8928bc90827aa6b49654484428282acb
Brad Nelson <flagxor@gmail.com>
2015-08-21 21:35:31 -0700

    Switch to standard audio class.

--- forthsalon/appengine/static/haiku.js
+++ forthsalon/appengine/static/haiku.js
@@ -807,7 +807,7 @@ function synth(n, t) {
 // Setup audio pipeline.
 var audio_context;
 try {
-  audio_context = new webkitAudioContext();
+  audio_context = new AudioContext();
 } catch (e) {
 }
 var audio_off = function(t, x) { return [0, 0, 0, 1]; };
